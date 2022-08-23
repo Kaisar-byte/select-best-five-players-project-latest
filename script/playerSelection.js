8fb7d5d1e3d622c612fedad53288f798691cb0f9
mohammadkaisarhamid <mohammadkaisarhamid@gmail.com>
2022-08-23 21:29:00 +0600

    players section job is going on

--- script/playerSelection.js
+++ script/playerSelection.js
@@ -1,6 +1,3 @@
 document.getElementById('btn-messi').addEventListener('click', function () {
     console.log('messi key is trigged')
 })
-document.getElementById('btn-messi').addEventListener('click', function () {
-    console.log('messi key is trigged')
-})
